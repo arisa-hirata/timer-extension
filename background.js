b9cc02a8d9e538b4b6f02fc20e62b073a4660c55
Arisa Hirata <arisa.pirata@gmail.com>
2021-05-31 21:51:42 -0700

    Chrome Notifications API

--- background.js
+++ background.js
@@ -1,21 +1,27 @@
 let time = 0;
 
 setInterval(() => {
-    time += 1;
-    console.log(time);
+	time += 1;
+	console.log(time);
 }, 1000);
 chrome.alarms.create({
-    periodInMinutes: 1 / 60,
+	periodInMinutes: 1 / 60,
 });
 
 chrome.alarms.onAlarm.addListener((alarm) => {
-    chrome.storage.local.get(["timer"], (res) => {
-        const time = res.timer ?? 0
-        chrome.storage.local.set({
-            timer: time + 1,
-        });
-        chrome.action.setBadgeText({
-            text: `${time + 1}`
-        });
-    });
+	chrome.storage.local.get(["timer"], (res) => {
+		const time = res.timer ?? 0
+		chrome.storage.local.set({
+			timer: time + 1,
+		});
+		chrome.action.setBadgeText({
+			text: `${time + 1}`
+		});
+		if (time % 1000 == 0) {
+			this.registration.showNotification("Chrome Timer Extension", {
+				body: "10 second has passed!",
+				icon: "icon.png",
+			})
+		}
+	});
 });
