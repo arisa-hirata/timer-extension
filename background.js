cfc91fb38d10ce8a63de985530763f456820e9d6
Arisa Hirata <arisa.pirata@gmail.com>
2021-05-31 22:04:09 -0700

    Notification Time Option Feature

--- background.js
+++ background.js
@@ -17,11 +17,14 @@ chrome.alarms.onAlarm.addListener((alarm) => {
 		chrome.action.setBadgeText({
 			text: `${time + 1}`
 		});
-		if (time % 1000 == 0) {
-			this.registration.showNotification("Chrome Timer Extension", {
-				body: "10 second has passed!",
-				icon: "icon.png",
-			})
-		}
+		chrome.storage.sync.get(["notificationTime"], (res) => {
+			const notificationTime = res.notificationTime ?? 1000;
+			if (time % notificationTime == 0) {
+				this.registration.showNotification("Chrome Timer Extension", {
+					body: `${notificationTime} second has passed!`,
+					icon: "icon.png",
+				})
+			}
+		});
 	});
 });
--- options.js
+++ options.js
@@ -1,15 +1,17 @@
 const nameInput = document.getElementById("name-input");
+const timeInput = document.getElementById("time-input");
 const saveBtn = document.getElementById("save-btn");
 
 saveBtn.addEventListener("click", () => {
-    const name = nameInput.value;
-    chrome.storage.sync.set({
-        name,
-    }, () => {
-        console.log(`name is set to ${name}`)
-    });
+	const name = nameInput.value;
+	const notificationTime = timeInput.value;
+	chrome.storage.sync.set({
+		name,
+		notificationTime,
+	});
 });
 
-chrome.storage.sync.get(["name"], (res) => {
-    nameInput.value = res.name ?? "???";
+chrome.storage.sync.get(["name", "notification"], (res) => {
+	nameInput.value = res.name ?? "???";
+	timeInput.value = res.notificationTime ?? 1000
 })
